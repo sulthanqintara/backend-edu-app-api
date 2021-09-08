7af20a3e2697245b1cc54e54d5a121d69f24a148
sulthanqintara <sulthanqintara@gmail.com>
2021-09-09 00:37:26 +0700

    add duplicate email when register

--- src/models/users.js
+++ src/models/users.js
@@ -1,25 +1,32 @@
-const bcrypt = require('bcrypt');
-const db = require('../database/mysql');
+const bcrypt = require("bcrypt");
+const db = require("../database/mysql");
 
 // CREATE NEW
-const createNewUser = (body) => new Promise((resolve, reject) => {
-  const qs = 'INSERT INTO users SET ?';
-  bcrypt.genSalt(10, (err, salt) => {
-    if (err) return reject(err);
-    bcrypt.hash(body.password, salt, (error, hash) => {
-      if (error) return reject(error);
-      const userData = {
-        ...body,
-        password: hash,
-      };
-      db.query(qs, userData, (err, result) => {
+const createNewUser = (body) =>
+  new Promise((resolve, reject) => {
+    const { email } = body;
+    const getQuery = "SELECT * FROM users WHERE email = ?";
+    db.query(getQuery, email, (err, result) => {
+      if (err) return reject(err);
+      if (result.length) return reject(409);
+      bcrypt.genSalt(10, (err, salt) => {
         if (err) return reject(err);
-        return resolve(result);
+        bcrypt.hash(body.password, salt, (error, hash) => {
+          if (error) return reject(error);
+          const userData = {
+            ...body,
+            password: hash,
+          };
+          const qs = "INSERT INTO users SET ?";
+          db.query(qs, userData, (err, result) => {
+            if (err) return reject(err);
+            return resolve(result);
+          });
+        });
       });
     });
   });
-});
 
 module.exports = {
-    createNewUser,
-}
+  createNewUser,
+};
--- src/routes/classes.js
+++ src/routes/classes.js
@@ -1,6 +1,7 @@
-const classRouter = require('express').Router();
-const classHandler = require('../handlers/classes');
+const classRouter = require("express").Router();
+const classHandler = require("../handlers/classes");
 
-classRouter.post('/', classHandler.addNewClass);
+/* http://localhost:8000/classes */
+classRouter.post("/", classHandler.addNewClass);
 
 module.exports = classRouter;
