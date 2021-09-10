b742ac6f3f552a41033a973d186abb33f24728f3
akbrrmdhn <akbar.rmdhn125@gmail.com>
2021-09-10 17:39:40 +0700

    added subject route, handlers, and models

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const mainRouter = require("express").Router();
 const userRouter = require("./users");
 const authRouter = require("./auth");
 const classRouter = require("./classes");
+const subjectRouter = require("./subjects");
 
 mainRouter.use("/auth", authRouter);
 mainRouter.use("/users", userRouter);
 mainRouter.use("/classes", classRouter);
+mainRouter.use("/subjects", subjectRouter);
 
 module.exports = mainRouter;
