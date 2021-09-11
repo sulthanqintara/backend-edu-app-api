cd0f57726bd79d8b7c5cdaf7a2934521226cc75a
akbrrmdhn <akbar.rmdhn125@gmail.com>
2021-09-11 12:27:20 +0700

    added class image & level table

--- src/handlers/classes.js
+++ src/handlers/classes.js
@@ -2,9 +2,9 @@ const classModel = require("../models/classes");
 const responseHelper = require("../helpers/response");
 
 const addNewClass = (req, res) => {
-  const { body } = req;
+  const { file, body } = req;
   classModel
-    .addNewClass(body)
+    .addNewClass(file, body)
     .then((data) => responseHelper.success(res, 201, data))
     .catch((err) => responseHelper.error(res, 500, err.message));
 };
--- src/models/classes.js
+++ src/models/classes.js
@@ -1,11 +1,32 @@
 const db = require("../database/mysql");
 
-const addNewClass = (body) =>
+const addNewClass = (file, body) =>
   new Promise((resolve, reject) => {
-    const queryString = "INSERT INTO classes SET ?";
-    db.query(queryString, body, (err, result) => {
+    const id = body.id;
+    const getFileQuery = "SELECT image FROM classes";
+
+    db.query(getFileQuery, id, (err, dbUrl) => {
       if (err) return reject(err);
-      return resolve(result);
+      let input;
+
+      if (file) {
+        const imageUrl = `/images/${file.filename}`;
+        input = {
+          image: imageUrl,
+        };
+      }
+      if (!file) {
+        input = {
+          image: dbUrl[0]?.image,
+        };
+      }
+
+      const newBody = { ...body, ...input };
+      const insertQuery = "INSERT INTO classes SET ?";
+      db.query(insertQuery, [newBody, id], (err, result) => {
+        if (err) return reject(err);
+        return resolve(result);
+      });
     });
   });
 
@@ -22,20 +43,20 @@ const getClasses = (query, hostname) =>
   new Promise((resolve, reject) => {
     const keyword = query?.keyword ? query.keyword : "";
     const category_id = query?.category_id ? query.category_id : 0;
-    const level = query?.level ? query.level : "";
+    const level_id = query?.level_id ? query.level_id : 0;
     const price = query?.price ? query.price : 0;
     const page = Number(query.page) || 1;
     const limit = Number(query.limit) || 10;
     const offset = limit * (page - 1);
-    const queryString = `SELECT * FROM classes WHERE name LIKE "%${keyword}%" AND category_id >= ? AND level LIKE "%${level}%" AND pricing >= ? LIMIT ? OFFSET ?`;
+    const queryString = `SELECT * FROM classes WHERE name LIKE "%${keyword}%" AND category_id >= ? AND level_id >= ? AND pricing >= ? LIMIT ? OFFSET ?`;
     db.query(
       queryString,
-      [category_id, price, limit, offset],
+      [category_id, level_id, price, limit, offset],
       (error, result) => {
         if (error) return reject(error);
         if (!result.length) return reject(404);
-        const queryCountTotal = `SELECT COUNT(id) AS total FROM classes WHERE name LIKE "%${keyword}%" AND category_id = ? AND level LIKE "%${level}%" AND pricing >= ?`;
-        db.query(queryCountTotal, [category_id, price], (err, totalResult) => {
+        const queryCountTotal = `SELECT COUNT(id) AS total FROM classes WHERE name LIKE "%${keyword}%" AND category_id = ? AND level_id >= ? AND pricing >= ?`;
+        db.query(queryCountTotal, [category_id, level_id, price], (err, totalResult) => {
           if (err) return reject(err);
           const totalData = totalResult[0].total;
           const totalPage = Math.ceil(totalData / limit);
@@ -48,9 +69,9 @@ const getClasses = (query, hostname) =>
           query.category_id &&
             ((urlPrevPage = urlPrevPage + `category_id=${category_id}&`),
             (urlNextPage = urlNextPage + `category_id=${category_id}&`));
-          query.level &&
-            ((urlPrevPage = urlPrevPage + `level=${level}&`),
-            (urlNextPage = urlNextPage + `level=${level}&`));
+          query.level_id &&
+            ((urlPrevPage = urlPrevPage + `level_id=${level_id}&`),
+            (urlNextPage = urlNextPage + `level_id=${level_id}&`));
           query.price &&
             ((urlPrevPage = urlPrevPage + `price=${price}&`),
             (urlNextPage = urlNextPage + `price=${price}&`));
