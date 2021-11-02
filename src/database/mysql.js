0f88ce953e4e0da631f0fb5817df3c38f2ba9413
sulthanqintara <sulthanqintara@gmail.com>
2021-11-03 00:21:22 +0700

    change to createpool mysql

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -1,9 +1,7 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
-const {
-  DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE,
-} = process.env;
-const db = mysql.createConnection({
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+const db = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
